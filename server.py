0863719eccfc22b17e8675319d9c1b43d48230ca
alfredzhuang <alfredzhuang123@gmail.com>
2021-05-24 14:29:24 -0700

    add home page

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '908oajsfoafh129y41298h4r12'
+Bootstrap(app)
 
 @app.route("/")
 def home():
