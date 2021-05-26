cc1b6fa2ed79f568130a6079e7ddcc684d2b7ec4
alfredzhuang <alfredzhuang123@gmail.com>
2021-05-26 14:45:00 -0700

    add API calls

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+import requests
+from fpdf import FPDF
+
+MEAL_DB_URL = "https://www.themealdb.com/api/json/v1/1/random.php"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '908oajsfoafh129y41298h4r12'
@@ -11,9 +15,22 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/recipe")
+@app.route("/recipe", methods=["GET", "POST"])
 def recipe():
-    return render_template('recipe.html')
+    response = requests.get(MEAL_DB_URL)
+    data = response.json()
+    name = data["meals"][0]["strMeal"]
+    category = data["meals"][0]["strCategory"]
+    area = data["meals"][0]["strArea"]
+    ingredients = []
+    for i in range(1, 21):
+        if data["meals"][0]["strIngredient" + str(i)] == "":
+            break
+        else:
+            ingredients.append(
+                data["meals"][0]["strMeasure" + str(i)] + " " + data["meals"][0]["strIngredient" + str(i)])
+    instructions = data["meals"][0]["strInstructions"]
+    return render_template('recipe.html', name=name, category=category, area=area, ingredients=ingredients, instructions=instructions)
 
 
 if __name__ == "__main__":
