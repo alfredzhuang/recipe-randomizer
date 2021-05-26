ccdcaaefc062105afb31c4a76d7b5862091f597e
alfredzhuang <alfredzhuang123@gmail.com>
2021-05-26 16:50:57 -0700

    add option for users to download instructions as pdf

--- server.py
+++ server.py
@@ -9,16 +9,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '908oajsfoafh129y41298h4r12'
 Bootstrap(app)
 
+name = ""
+category = ""
+area = ""
+ingredients = []
+instructions = ""
 
-@app.route("/")
-def home():
-    return render_template('index.html')
 
-
-@app.route("/recipe", methods=["GET", "POST"])
-def recipe():
+def get_recipe():
     response = requests.get(MEAL_DB_URL)
     data = response.json()
+    global name, category, area, ingredients, instructions
     name = data["meals"][0]["strMeal"]
     category = data["meals"][0]["strCategory"]
     area = data["meals"][0]["strArea"]
@@ -27,9 +28,26 @@ def recipe():
         if data["meals"][0]["strIngredient" + str(i)] == "":
             break
         else:
-            ingredients.append(
-                data["meals"][0]["strMeasure" + str(i)] + " " + data["meals"][0]["strIngredient" + str(i)])
-    instructions = data["meals"][0]["strInstructions"]
+            ingredients.append(data["meals"][0]["strMeasure" + str(i)] + " " + data["meals"][0]["strIngredient" + str(i)])
+    instructions = data["meals"][0]["strInstructions"].encode('latin-1', 'replace').decode('latin-1')
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/recipe", methods=["GET", "POST"])
+def recipe():
+    global name, category, area, ingredients, instructions
+    if request.method == "POST":
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_font("Arial", size=12)
+        pdf.multi_cell(0, 5, txt=instructions, align="C")
+        pdf.output("instructions.pdf")
+        return render_template('recipe.html', name=name, category=category, area=area, ingredients=ingredients, instructions=instructions)
+    get_recipe()
     return render_template('recipe.html', name=name, category=category, area=area, ingredients=ingredients, instructions=instructions)
 
 
