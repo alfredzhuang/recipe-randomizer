b5ab14e1c7d71101e21169b765c8873e64a773d4
alfredzhuang <alfredzhuang123@gmail.com>
2021-05-31 13:45:32 -0700

    add purchase ingredients option

--- server.py
+++ server.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 import requests
 from fpdf import FPDF
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+import time
 
 MEAL_DB_URL = "https://www.themealdb.com/api/json/v1/1/random.php"
 
@@ -35,6 +39,32 @@ def get_recipe():
     instructions = instructions.replace(u'\u2019', "'")
 
 
+def automated_cart(list_of_ingredients):
+    chrome_drive_path = "C:\Development\chromedriver.exe"
+    driver = webdriver.Chrome(executable_path=chrome_drive_path)
+    driver.maximize_window()
+    driver.get("https://www.walmart.com/grocery")
+    time.sleep(2)
+    close_button = driver.find_element_by_xpath("html/body/div[2]/div/div/div/button")
+    close_button.click()
+    for ingredient in list_of_ingredients:
+        try:
+            search_field = driver.find_element_by_xpath("//*[@id='searchForm']/input")
+            search_field.send_keys(str(ingredient))
+            search_field.send_keys(Keys.ENTER)
+
+            time.sleep(2)
+            first_item = driver.find_element_by_class_name("AddToCart__addToCart___D6vAa")
+            first_item.click()
+
+            clear_button = driver.find_element_by_xpath("//*[@id='searchForm']/button[1]")
+            clear_button.click()
+        except NoSuchElementException:
+            print("Couldn't find the ingredient")
+            continue
+    time.sleep(600)
+
+
 @app.route("/")
 def home():
     return render_template('index.html')
@@ -51,6 +81,13 @@ def recipe():
         pdf.output(name="instructions.pdf")
         return render_template('recipe.html', name=name, category=category, area=area, ingredients=ingredients, instructions=instructions)
     get_recipe()
+    return render_template('recipe.html', name=name, category=category, area=area, ingredients=ingredients, instructions=instructions)
+
+
+@app.route("/add-to-cart", methods=["POST"])
+def add_to_cart():
+    global name, category, area, ingredients, instructions
+    automated_cart(ingredients)
     return render_template('recipe.html', name=name, category=category, area=area, ingredients=ingredients, instructions=instructions)
 
 
